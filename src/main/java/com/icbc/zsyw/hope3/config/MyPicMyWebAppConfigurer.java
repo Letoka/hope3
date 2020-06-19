b87904a883170d1d225875399a1f9dc6c40a76ec
qinwankang <273509825@qq.com>
2020-06-19 09:20:05 +0800

    图片映射路径，增加Linux版本映射

--- src/main/java/com/icbc/zsyw/hope3/config/MyPicMyWebAppConfigurer.java
+++ src/main/java/com/icbc/zsyw/hope3/config/MyPicMyWebAppConfigurer.java
@@ -15,7 +15,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyPicMyWebAppConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //外部访问路径映射到本地磁盘路径
+        //外部访问路径映射到本地磁盘路径--windows系统映射
         registry.addResourceHandler("/static/upload/**").addResourceLocations("file:D:\\icbc\\image\\");
+        //外部访问路径映射到本地磁盘路径--Linux系统映射1
+     //   registry.addResourceHandler("/static/upload/**").addResourceLocations("/data/tmp/image/");
+        //外部访问路径映射到本地磁盘路径--Linux系统映射2
+        //   registry.addResourceHandler("/static/upload/**").addResourceLocations("/data/tmp/image");
     }
 }
